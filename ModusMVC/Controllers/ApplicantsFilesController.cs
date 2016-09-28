3ceb865f355015fc88de2b7ce2a1d0e9cdd8a54b
Tebogo Mokasola <tcmokasola@gmail.com>
2016-09-28 08:20:15 +0200

    updated version

---
+++ ModusMVC/Controllers/ApplicantsFilesController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModusMVC.Controllers
+{
+    public class ApplicantsFilesController : Controller
+    {
+        // GET: ApplicantsFiles
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Graduates()
+        {
+            try
+            {
+                var dir = new System.IO.DirectoryInfo(Server.MapPath("~/CV/Graduate/Software Development"));
+                List<string> items = new List<string>();
+
+                System.IO.FileInfo[] fileNames = dir.GetFiles("*.zip");
+
+                foreach (var file in fileNames)
+                {
+                    items.Add(file.Name);
+                }
+
+                return View(items);
+            }
+            catch (Exception)
+            {
+
+                return View("NotFound");
+            }
+        }
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+        public FileResult Download(string ImageName)
+        {
+            //      return  File("~/CV/Graduate/7/" + filename, System.Net.Mime.MediaTypeNames.Application.Octet);
+
+
+            var filepath = System.IO.Path.Combine(Server.MapPath("~/CV/Graduate/Software Development"), ImageName);
+            return File(filepath, MimeMapping.GetMimeMapping(filepath), ImageName);
+
+
+
+        }
+
+        /////////////////////////////////////////
+        public ActionResult InternshipSQA()
+        {
+            try
+            {
+                var dir = new System.IO.DirectoryInfo(Server.MapPath("~/CV/Internship/Software Quality Assuarence"));
+                List<string> items = new List<string>();
+
+                System.IO.FileInfo[] fileNames = dir.GetFiles("*.zip");
+
+                foreach (var file in fileNames)
+                {
+                    items.Add(file.Name);
+                }
+
+                return View(items);
+            }
+            catch (Exception)
+            {
+
+                return View("NotFound");
+            }
+        }
+        public ActionResult NotFoundInternSQA()
+        {
+            return View();
+        }
+        public FileResult DownloadInternSQA(string ImageName)
+        {
+            //      return  File("~/CV/Graduate/7/" + filename, System.Net.Mime.MediaTypeNames.Application.Octet);
+
+
+            var filepath = System.IO.Path.Combine(Server.MapPath("~/CV/Internship/Software Quality Assuarence"), ImageName);
+            return File(filepath, MimeMapping.GetMimeMapping(filepath), ImageName);
+
+
+
+        }
+
+        /////////////////////////////////////////
+        public ActionResult InternshipSD()
+        {
+            try
+            {
+                var dir = new System.IO.DirectoryInfo(Server.MapPath("~/CV/Internship/Software Development"));
+                List<string> items = new List<string>();
+
+                System.IO.FileInfo[] fileNames = dir.GetFiles("*.zip");
+
+                foreach (var file in fileNames)
+                {
+                    items.Add(file.Name);
+                }
+
+                return View(items);
+            }
+            catch (Exception)
+            {
+
+                return View("NotFound");
+            }
+        }
+        public ActionResult NotFoundInternSD()
+        {
+            return View();
+        }
+        public FileResult DownloadInternSD(string ImageName)
+        {
+            var filepath = System.IO.Path.Combine(Server.MapPath("~/CV/Internship/Software Development"), ImageName);
+            return File(filepath, MimeMapping.GetMimeMapping(filepath), ImageName);
+        }
+
+        /////////////////////////////////////////
+        public ActionResult LearnershipSD()
+        {
+            try
+            {
+                var dir = new System.IO.DirectoryInfo(Server.MapPath("~/CV/Learnership/Software Development"));
+                List<string> items = new List<string>();
+
+                System.IO.FileInfo[] fileNames = dir.GetFiles("*.zip");
+
+                foreach (var file in fileNames)
+                {
+                    items.Add(file.Name);
+                }
+
+                return View(items);
+            }
+            catch (Exception)
+            {
+
+                return View("NotFound");
+            }
+        }
+        public ActionResult NotFoundLearnershipSD()
+        {
+            return View();
+        }
+        public FileResult DownloadLearnershipSD(string ImageName)
+        {
+            var filepath = System.IO.Path.Combine(Server.MapPath("~/CV/Learnership/Software Development"), ImageName);
+            return File(filepath, MimeMapping.GetMimeMapping(filepath), ImageName);
+        }
+    }
+}
---
+++ ModusMVC/Controllers/CascadingDropDownSampleController.cs
@@ -0,0 +1,119 @@
+﻿/*********************************** Module Header ***********************************\
+Module Name:  CascadingDropDownSampleController.cs
+Project:      CSDropdownListMVC4
+Copyright (c) Microsoft Corporation.
+
+ASP.NET MVC 4 is a framework for building scalable, standards-based web applications using well-established design patterns and the power of ASP.NET and the .NET Framework. 
+This article and the attached code samples demonstrate demonstrates how to use cascading dropdown list with ASP.NET MVC 4. 
+
+This source is subject to the Microsoft Public License.
+See http://www.microsoft.com/opensource/licenses.mspx#Ms-PL.
+All other rights reserved.
+
+THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER 
+EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF 
+MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+\*************************************************************************************/
+
+#region "Directives"
+
+using CSDropdownListMVC4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+#endregion
+
+namespace CSDropdownListMVC4.Controllers
+{
+    /// <summary>
+    /// Controller class for sample
+    /// </summary>
+    public class CascadingDropDownSampleController : Controller
+    {
+        #region "Public Actions"
+
+        /// <summary>
+        /// Default Action for the web-page handling HTTP GET requests
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Index()
+        {
+            IDictionary<string, string> makes = GetSampleMakes();
+            CascadingDropDownSampleModel viewModel = new CascadingDropDownSampleModel()
+            {
+                Makes = makes
+            };
+
+            return View(viewModel);
+        }
+
+        /// <summary>
+        /// AJAX Action to send sample Models in JSON format based on the selected make
+        /// </summary>
+        /// <param name="selectedMake"></param>
+        /// <returns></returns>
+        public ActionResult GetSampleModels(string selectedMake)
+        {
+            IDictionary<string, string> models = GetSampleModelsFromSelectedMake(selectedMake);
+      
+            return Json(models);
+        }
+
+        #endregion
+
+        #region "Private Methods"
+
+        /// <summary>
+        /// Method to generate sample makes
+        /// </summary>
+        /// <returns></returns>
+        private IDictionary<string, string> GetSampleMakes()
+        {
+            IDictionary<string, string> makes = new Dictionary<string, string>();
+
+            makes.Add("1", "Acura");
+            makes.Add("2", "Audi");
+            makes.Add("3", "BMW");
+
+            return makes;
+        }
+
+        /// <summary>
+        /// Method to generate sample models based on selected make
+        /// </summary>
+        /// <param name="selectedMake"></param>
+        /// <returns></returns>
+        private IDictionary<string, string> GetSampleModelsFromSelectedMake(string selectedMake)
+        {
+            IDictionary<string, string> models = new Dictionary<string, string>();
+
+            switch (selectedMake)
+            {
+                case "1":
+                    models.Add("1", "Software Development");
+                    models.Add("2", "Software Quality Assuarence");
+                    break;
+                case "2":
+                    models.Add("4", "Software Delvelopment");
+                    break;
+                case "3":
+                    models.Add("7", "Software Development");
+                    break;
+                default:
+                    throw new NotImplementedException();
+
+            }
+
+            return models;
+        }
+
+      
+
+        #endregion
+
+    }
+}
